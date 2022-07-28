8ef2c20a51b1e6ec0eb2178cd37bea205d227eec
nayyara-airlangga <nayyara-airlangga@gmail.com>
2022-07-28 22:12:59 +0700

    feat: add orgcodes and org structs

---
+++ src/orgs/mod.rs
@@ -0,0 +1,20 @@
+use std::{collections::HashMap, fs};
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Serialize)]
+pub struct Organization {
+    pub faculty: String,
+
+    #[serde(rename = "shortFaculty")]
+    pub short_faculty: String,
+
+    pub major: String,
+    pub program: String,
+}
+
+pub fn get_organizations() -> HashMap<String, Organization> {
+    let json = fs::read_to_string("./src/orgs/orgcode.json").unwrap();
+
+    serde_json::from_str(&json).unwrap()
+}
